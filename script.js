00c52c42164ef0639f873e55c44bafad77da0174
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2024-09-15 01:11:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//menu-bar 
+var menu=document.getElementById("menu");
+var menuBox=document.getElementById("menu-bar");
+var cross=document.querySelector(".cross");
+var menutick=false;
+function crossBar(){
+    menuBox.style.display="none";
+    menutick=false;
+}
+function menuBar(){
+    if(menutick==false){
+        menuBox.style.display="flex";
+        menutick=true;
+    }
+    else{
+        menuBox.style.display="none";
+        menutick=false;
+    }
+}
+function menuBarClose(){
+    menuBox.style.display="none";
+    menutick=false;
+}
